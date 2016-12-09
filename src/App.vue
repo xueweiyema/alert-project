1148efe3ec0d306e83c1fc0612bd955368111042
xueweiyema@yahoo.com.cn <123456>
2016-12-09 16:45:32 +0800

    alert-component
    alert-component

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div id="app">
+  <Alert v-bind:alertShow="as" v-bind:message="msg" v-on:closeAlert="closeAlert"></Alert>
+  <button class='btn btn-default' v-on:click='openAlert'>open alert</button>
+</div>
+</template>
+
+<script>
+import Alert from './components/Alert'
+
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      msg: 'I am a alert !!!',
+      as: true
+    }
+  },
+  methods: {
+    openAlert: function() {
+      this.as = true;
+    },
+    closeAlert: function() {
+      this.as = false;
+    }
+  },
+  components: {
+    Alert
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Alert.vue
@@ -0,0 +1,28 @@
+<template>
+<div v-show='alertShow' class="alert alert-danger alert-dismissible ">
+  <span class="close" aria-label="close" v-on:click='closeAlert'>×</span> {{ message }}
+</div>
+</template>
+
+<script>
+export default {
+  props: ['message','alertShow'],
+
+  methods: {
+    closeAlert: function() {
+      this.$emit('closeAlert')
+    }
+  },
+}
+</script>
+
+<style>
+/*.alert {
+    padding: 10px;
+    background: #f0f0f0;
+  }
+  .alert-error {
+    background: red;
+    color: white;
+  }*/
+</style>
